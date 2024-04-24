56668633123a53d26aefea6c87a400ba3fff3929
git <rakeshbalaji21799@gmail.com>
2024-04-23 21:03:59 -0700

    Made changes to app.js

--- docs/app.js
+++ docs/app.js
@@ -1,29 +1,28 @@
 (function () {
     'use strict';
-
+  
     angular.module('LunchCheck', [])
-    .controller('LunchCheckController', LunchCheckController);
-
-    LunchCheckController.$inject = ['$scope'];
-
-    function LunchCheckController($scope) {
-        var ctrl = this;
-
-        ctrl.items = '';
-        ctrl.message = '';
-
-        ctrl.checkItems = function () {
-            if (!ctrl.items) {
-                ctrl.message = 'Please enter data first';
-                return;
-            }
-
-            var itemCount = ctrl.items.split(',')
-                             .filter(function (item) {
-                                 return item.trim() !== '';
-                             }).length;
-
-            ctrl.message = itemCount <= 3 ? 'Enjoy!' : 'Too much!';
-        };
+      .controller('LunchCheckController', LunchCheckController);
+  
+    function LunchCheckController() {
+      var ctrl = this;
+  
+      ctrl.items = '';
+      ctrl.message = '';
+  
+      ctrl.checkItems = function () {
+        if (!ctrl.items.trim()) {
+          ctrl.message = "Please enter data first";
+          return;
+        }
+  
+        var itemCount = ctrl.items.split(',')
+          .filter(function (item) {
+            return item.trim() !== '';
+          }).length;
+  
+        ctrl.message = itemCount <= 3 ? "Enjoy!" : "Too much!";
+      };
     }
-})();
+  })();
+  
